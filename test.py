0de005f8265747e52d41f20d3984edddaf3621d0
Wayne T Taylor <wayne@kryptos.co.uk>
2022-04-19 17:48:51 +0100

    Images folder, test.py changes

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import RPi.GPIO as gpio
 import time
 
+# Set Mode
 gpio.setmode(gpio.BCM)
-gpio.setup(23, gpio.OUT)
 
-gpio.output(23, gpio.LOW)
-time.sleep(4)
-gpio.output(23, gpio.HIGH)
+# Set Pin 18 as Output and Turn off // Turn OFF RGBW ring
+gpio.setup(18, gpio.OUT)
+gpio.output(18, gpio.OFF)
+
+gpio.setup(23, gpio.OUT)
+gpio.output(23, gpio.OFF)
+# gpio.output(23, gpio.HIGH)
 
 gpio.setup(24, gpio.OUT)
+gpio.output(24, gpio.OFF)
+#gpio.output(24, gpio.HIGH)
 
-gpio.output(24, gpio.LOW)
-time.sleep(4)
-gpio.output(24, gpio.HIGH)
